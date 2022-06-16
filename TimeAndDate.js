1c59c63f87d72eba4c88380fae1768c992bf0964
Your Name <you@example.com>
2022-06-16 15:53:31 +0500

    add node module and export other file and make server which show current time

--- TimeAndDate.js
+++ TimeAndDate.js
@@ -1,1 +1,17 @@
-exports.timeanddate()
+const http = require("http");
+var dt = require('./ModuleImport');
+
+const hostName = "127.0.0.1";
+const port = 3000;
+
+
+
+var CheckServer = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end();
+})
+
+CheckServer.listen(port , hostName , ()=>{
+    console.log (`sever started at http://${hostName}:${port}`);
+});
