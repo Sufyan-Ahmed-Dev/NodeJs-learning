e447e60f95a9bbe4b885708a21d5454b79fd3d23
Your Name <you@example.com>
2022-06-20 17:21:59 +0500

    html changes checking just

--- FirstNodeClass.js
+++ FirstNodeClass.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 const server = http.createServer((req,res)=>{
 res.statusCode = 200;
-res.setHeader("content-type" , "text/plane");
+res.setHeader("content-type" , "text/html");
 res.end("Hello World this is my first node server");
 
 })
