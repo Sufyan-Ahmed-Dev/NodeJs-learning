642c6326cc5a921350fccb9eb5f590fb32bf84d5
Your Name <you@example.com>
2022-06-20 17:27:55 +0500

    practice folder have all files

---
+++ practice work/basicServer.js
@@ -0,0 +1,14 @@
+// const http = require("http");
+// const hostName = "127.0.0.1";
+// const port = 4000;
+
+// var server = http.createServer((req,res)=>{
+//     res.statusCode = 200;
+//     res.setHeader("content-type", "text/html");
+//     res.write("hello hi how are you");
+//     res.end()
+// })
+
+// server.listen(port, hostName,()=>{
+//       console.log(`server start at this server http://${hostName}:${port}`)
+// })
